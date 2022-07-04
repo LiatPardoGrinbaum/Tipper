5f4fd5f1d941dbad46dc09f8c092fde04fdb2984
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-07-04 22:43:42 +0300

    upload to heroku

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ import express from "express";
 const fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileName);
 const publicDirPath = path.join(__dirname, "client/build");
-console.log(__dirname);
+console.log("dirname", __dirname);
 console.log(publicDirPath);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-// });
 const PORT = process.env.PORT || 5050;
 
 //pay attention to the location of this:
-app.use(express.static(__dirname + "/client/build"));
+app.use(express.static(publicDirPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicDirPath, "index.html"));
+});
 
 app.listen(PORT, (error) => {
   if (error) throw new Error("app.listen Error: " + error);
